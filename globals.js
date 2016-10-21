78a1bfba7c05ee2f056264a7d2780c06bcb8916a
elopezf <elopez@mahisoft.com>
2016-10-21 17:02:08 -0400

    Changed to select between 11 random zipcodes.
    Could be changed to more, later

--- globals.js
+++ globals.js
@@ -2,7 +2,7 @@ var variables = {
     user: "elopez@mahisoft.com",
     password: "123456",
     dir: '9224 W Lone Cactus Dr, Peoria, AZ 85382',
-    zip: '80031',
+    zipcodes: ['90002', '92602', '92618', '92620', '92672', '92688', '92037', '93636', '94107', '95628', '94536'],
     path: './tests_output/screenshots/',
     ext: '.png',
     sqft: '2000',
@@ -19,9 +19,9 @@ var datetime = today.toLocaleString();
 datetime = datetime.replace(/:|\s/g, '');
 datetime = datetime.replace(/['/']/g, '');
 
-/*var HtmlReporter = require('nightwatch-html-reporter');
+var HtmlReporter = require('nightwatch-html-reporter');
 
-
+/*
 var reporter = new HtmlReporter({
     openBrowser: false,
     reportsDirectory: "./tests_output/reports",
@@ -30,10 +30,9 @@ var reporter = new HtmlReporter({
     themeName: "default",
 
 
-});
-*/
+});*/
 
 module.exports = {
-  //  reporter: reporter.fn,
+    // reporter: reporter.fn,
     variables: variables
 }
